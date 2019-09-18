c4a031751995d0e810b725a10ab0dae055f75527
Abraham Isaac Durán <aid.mest@gmail.com>
2019-09-18 03:47:23 -0400

    Add unit tests

--- TextSaverTests/TextSaverTests.m
+++ TextSaverTests/TextSaverTests.m
@@ -7,31 +7,108 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "TextSaverViewModel.h"
 
 @interface TextSaverTests : XCTestCase
+
+@property (nonatomic) TextSaverViewModel *viewModel;
 
 @end
 
 @implementation TextSaverTests
+@synthesize viewModel;
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    viewModel = [[TextSaverViewModel alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    viewModel = nil;
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testViewModel_TextLargerThan140Chars_shouldBeInvalid {
+    // Arrange
+    NSString *longText = @"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et mag";
+    
+    // Asssert
+    XCTAssertFalse([viewModel isTextValid:longText]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testViewModel_TextShorterThan141Chars_shouldBeValid {
+    // Arrange
+    NSString *shortText = @"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et ma";
+    
+    // Asssert
+    XCTAssertTrue([viewModel isTextValid:shortText]);
+}
+
+- (void)testViewModel_TextWithHTMLTags_shouldBeInvalid {
+    // Arrange
+    NSString *htmlText = @"<html> <body> <p>Hello, World</p> </body> </html>";
+    
+    // Asssert
+    XCTAssertFalse([viewModel isTextValid:htmlText]);
+}
+
+- (void)testViewModel_TextWithNoHTMLTags_shouldBeValid {
+    // Arrange
+    NSString *nohtmlText = @"html body pHello, Worldp body html";
+    
+    // Asssert
+    XCTAssertTrue([viewModel isTextValid:nohtmlText]);
+}
+
+- (void)testViewModel_SanitizingTextWithHTMLTags_shouldRemoveHTMLChars {
+    // Arrange
+    NSString *htmlText = @"<html> <body> <p>Hello, World</p> </body> </html>";
+    NSString *expected = @"html body pHello, Worldp body html";
+    
+    // Act
+    NSString *result = [viewModel sanitizeText:htmlText];
+    
+    // Assert
+    XCTAssertTrue([result isEqualToString:expected]);
+}
+
+- (void)testViewModel_Saving_shouldNotAllowInvalidText {
+    // Arrange
+    NSString *invalidText = @"<html> <body> <p>Hello, World</p> </body> </html>";
+    NSInteger expected = 0;
+    
+    // Act
+    [viewModel saveText:invalidText];
+    NSArray *result = [viewModel getSavedTexts];
+    
+    // Assert
+    XCTAssertEqual(result.count, expected);
+}
+
+- (void)testViewModel_Saving_shouldAllowValidText {
+    // Arrange
+    NSString *validText = @"Hello, World";
+    NSInteger expected = 1;
+    
+    // Act
+    [viewModel saveText:validText];
+    NSArray *result = [viewModel getSavedTexts];
+    
+    // Assert
+    XCTAssertEqual(result.count, expected);
+    XCTAssertTrue([[result firstObject] isEqualToString:validText]);
+}
+
+- (void)testViewModel_Removing_shouldRemove {
+    // Arrange
+    NSString *validText = @"Hello, World";
+    NSInteger expected = 0;
+    
+    // Act
+    [viewModel saveText:validText];
+    [viewModel removeTextAtIndex:0];
+    NSArray *result = [viewModel getSavedTexts];
+    
+    // Assert
+    XCTAssertEqual(result.count, expected);
 }
 
 @end
